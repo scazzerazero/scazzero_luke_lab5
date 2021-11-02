63bd2a6120c0ebf33a78515f0ed0309145208cdf
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-02 21:59:42 +0000

    no .py

--- lab5_background.py
+++ lab5_background.py
@@ -3,7 +3,7 @@ import time
 import RPi.GPIO as GPIO 
 GPIO.setwarnings(False) 
 
-from stepper_class.py import Stepper
+from stepper_class import Stepper
 
 StepperObject=Stepper([18,21,22,23])# controller inputs: in1, in2, in3, in4
 
