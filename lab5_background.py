4442b28fd51eb8f2cbf8f8d0408eb97e9fdd6602
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-02 06:25:14 +0000

    not casting anything to value

--- lab5_background.py
+++ lab5_background.py
@@ -56,7 +56,7 @@ while True:
       form=json.load(f)
       time.sleep(0.1)
     
-    moveSteps(int(form['angleVal']),1) #512*8 is 1 step in the ccw direction.
+    moveSteps(form['angleVal'],1) #512*8 is 1 step in the ccw direction.
     if str(form['zerobutton'])=="ZeroMotor":
       print("ZERO DAT BITCH PLEASE")
 
