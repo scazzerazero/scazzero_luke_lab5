706fa5145f430c712668e44516277bcde35e186d
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-02 23:47:34 +0000

    adc.read print

--- stepper_class.py
+++ stepper_class.py
@@ -32,7 +32,8 @@ class Stepper:
     self.angle=targetAngle #the current angle is now the angle we just moved to!
     
   def Zero(self):
-    while self.ADC.read(0)<100 : #channel zero reads photoresistor value. Less light = higher val
+    while self.ADC.read(0)<500 : #channel zero reads photoresistor value. more light = lower val
+      print("we stisfied the condition! ADCread= "+str(self.ADC.read(0)))
       GPIO.output(self.ledPin, GPIO.HIGH)
       self.__halfstep(1)
 
