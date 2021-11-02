3c1dd152064d544c1cef878e793e87c1e658ec12
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-02 06:31:26 +0000

    add angle to steps conversion

--- lab5_background.py
+++ lab5_background.py
@@ -54,9 +54,10 @@ while True:
   try:
     with open("lab5_text.txt",'r') as f:
       form=json.load(f)
-      time.sleep(0.1)
-    
-    moveSteps(form['angleVal'],1) #512*8 is 1 step in the ccw direction.
+      time.sleep(0.3)
+    angle=float(form['angleVal'])
+    stepsReq=angle*(512*8)/(2*3.1415)
+    moveSteps(stepsReq,1) #512*8 is 1 rev in the ccw direction.
     if str(form['zerobutton'])=="ZeroMotor":
       print("ZERO DAT BITCH PLEASE")
 
