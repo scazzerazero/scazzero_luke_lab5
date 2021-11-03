c5c9abed21990a32d077b98cfa2f65456ba3b60a
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-03 00:52:33 +0000

    check

--- stepper_class.py
+++ stepper_class.py
@@ -29,6 +29,7 @@ class Stepper:
     stepsReq=diff*(512*8)/(360) #512*8 is 1 rev in the ccw direction.
     sign = lambda x: (1, -1)[x<0]
     print("self angle before= "+str(self.angle))
+    print("going into move steps= "+str(int(abs(stepsReq)),sign(diff)))
     self.__moveSteps(int(abs(stepsReq)),sign(diff)) #steps required, direction (+/- 1)
     self.__delay_us(10000)         
     self.angle=targetAngle #the current angle is now the angle we just moved to!
